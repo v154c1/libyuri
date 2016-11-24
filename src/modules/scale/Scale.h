2ea76cd569b02585104d356aad24d8d3ad3ab0e5
Zdenek Travnicek <travnicek@iim.cz>
2016-11-24 17:34:43 +0000

    Added support for events in scale module

--- src/modules/scale/Scale.h
+++ src/modules/scale/Scale.h
@@ -12,25 +12,27 @@
 
 #include "yuri/core/thread/SpecializedIOFilter.h"
 #include "yuri/core/frame/RawVideoFrame.h"
+#include "yuri/event/BasicEventConsumer.h"
 
 namespace yuri {
 namespace scale {
 
-class Scale: public core::SpecializedIOFilter<core::RawVideoFrame>
-{
-	using base_type = core::SpecializedIOFilter<core::RawVideoFrame>;
-public:
-	IOTHREAD_GENERATOR_DECLARATION
-	static core::Parameters configure();
-	Scale(const log::Log &log_, core::pwThreadBase parent, const core::Parameters &parameters);
-	virtual ~Scale() noexcept;
-private:
-	
-	virtual core::pFrame do_special_single_step(core::pRawVideoFrame frame) override;
-	virtual bool set_param(const core::Parameter& param) override;
+class Scale : public core::SpecializedIOFilter<core::RawVideoFrame>, public event::BasicEventConsumer {
+    using base_type = core::SpecializedIOFilter<core::RawVideoFrame>;
 
-	resolution_t resolution_;
-	bool fast_;
+public:
+    IOTHREAD_GENERATOR_DECLARATION
+    static core::Parameters configure();
+    Scale(const log::Log& log_, core::pwThreadBase parent, const core::Parameters& parameters);
+    virtual ~Scale() noexcept;
+
+private:
+    virtual core::pFrame do_special_single_step(core::pRawVideoFrame frame) override;
+    virtual bool set_param(const core::Parameter& param) override;
+    virtual bool do_process_event(const std::string& event_name, const event::pBasicEvent& event) override;
+
+    resolution_t resolution_;
+    bool         fast_;
 };
 
 } /* namespace scale */
