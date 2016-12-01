57672e16d7297c61c2870df426b7297dae1cc5a9
Zdenek Travnicek <travnicek@iim.cz>
2016-12-01 18:33:34 +0000

    Formate webserver/common_types.h

--- src/modules/webserver/common_types.h
+++ src/modules/webserver/common_types.h
@@ -17,59 +17,52 @@
 namespace yuri {
 namespace webserver {
 enum class http_code {
-	continue_ = 100,
-	ok = 200,
-	created = 201,
-	accepted = 202,
-	no_content = 204,
-	partial = 206,
-	moved = 301,
-	found = 302,
-	see_other = 303,
-	not_modified = 304,
-	bad_request = 400,
-	unauthorized = 401,
-	forbidden = 403,
-	not_found = 404,
-	gone = 410,
-	server_error = 500,
-	service_unavailable = 503
+    continue_           = 100,
+    ok                  = 200,
+    created             = 201,
+    accepted            = 202,
+    no_content          = 204,
+    partial             = 206,
+    moved               = 301,
+    found               = 302,
+    see_other           = 303,
+    not_modified        = 304,
+    bad_request         = 400,
+    unauthorized        = 401,
+    forbidden           = 403,
+    not_found           = 404,
+    gone                = 410,
+    server_error        = 500,
+    service_unavailable = 503
 };
-
 
 using parameters_t = std::map<std::string, std::string>;
 
 struct url_t {
-	url_t(std::string path={}, std::string host={}, parameters_t params=parameters_t{}, std::string anchor={}):
-		path(std::move(path)),host(std::move(host)),params(std::move(params)),anchor(std::move(anchor)) {}
-	~url_t() noexcept = default;
-	std::string path;
-	std::string host;
-	parameters_t params;
-	std::string anchor;
+    url_t(std::string path = {}, std::string host = {}, parameters_t params = parameters_t{}, std::string anchor = {})
+        : path(std::move(path)), host(std::move(host)), params(std::move(params)), anchor(std::move(anchor))
+    {
+    }
+    ~url_t() noexcept = default;
+    std::string  path;
+    std::string  host;
+    parameters_t params;
+    std::string  anchor;
 };
 
-struct request_t
-{
-	url_t url;
-	parameters_t parameters;
-	std::string method;
-	core::socket::pStreamSocket client;
+struct request_t {
+    url_t                       url;
+    parameters_t                parameters;
+    std::string                 method;
+    core::socket::pStreamSocket client;
 };
 
-struct response_t
-{
-	http_code code;
-	parameters_t parameters;
-	std::string data;
+struct response_t {
+    http_code    code;
+    parameters_t parameters;
+    std::string  data;
 };
-
-
-
 }
 }
-
-
-
 
 #endif /* SRC_MODULES_WEBSERVER_COMMON_TYPES_H_ */
