8ee8a5422d9fd2dcb2061f0da575de1b1c1a220a
Zdenek Travnicek <travnicek@iim.cz>
2016-12-01 15:39:38 +0000

    Formated webcontrol resource

--- src/modules/webserver/WebControlResource.cpp
+++ src/modules/webserver/WebControlResource.cpp
@@ -15,26 +15,27 @@
 namespace yuri {
 namespace webserver {
 
-
 IOTHREAD_GENERATOR(WebControlResource)
-
 
 core::Parameters WebControlResource::configure()
 {
-	core::Parameters p = core::IOThread::configure();
-	p.set_description("WebControlResource");
-	p["server_name"]["Name of server"]="webserver";
-	p["path"]["Path prefix"]="/control";
-	p["redirect"]["Redirect target"]="/";
-	return p;
+    core::Parameters p = core::IOThread::configure();
+    p.set_description("WebControlResource");
+    p["server_name"]["Name of server"] = "webserver";
+    p["path"]["Path prefix"]           = "/control";
+    p["redirect"]["Redirect target"]   = "/";
+    return p;
 }
 
-
-WebControlResource::WebControlResource(const log::Log &log_, core::pwThreadBase parent, const core::Parameters &parameters):
-core::IOThread(log_,parent,1,1,std::string("web_control")),WebResource(log),BasicEventProducer(log),
-server_name_("webserver"),path_("/control"),redirect_path_("/")
+WebControlResource::WebControlResource(const log::Log& log_, core::pwThreadBase parent, const core::Parameters& parameters)
+    : core::IOThread(log_, parent, 1, 1, std::string("web_control")),
+      WebResource(log),
+      BasicEventProducer(log),
+      server_name_("webserver"),
+      path_("/control"),
+      redirect_path_("/")
 {
-	IOTHREAD_INIT(parameters)
+    IOTHREAD_INIT(parameters)
 }
 
 WebControlResource::~WebControlResource() noexcept
@@ -43,49 +44,43 @@ WebControlResource::~WebControlResource() noexcept
 
 void WebControlResource::run()
 {
-	while (still_running() && !register_to_server(server_name_, path_, std::dynamic_pointer_cast<WebResource>(get_this_ptr()))) {
-		sleep(10_ms);
-	}
-	log[log::info] << "Registered to server";
-	while (still_running()) {
-		sleep(100_ms);
-	}
+    while (still_running() && !register_to_server(server_name_, path_, std::dynamic_pointer_cast<WebResource>(get_this_ptr()))) {
+        sleep(10_ms);
+    }
+    log[log::info] << "Registered to server";
+    while (still_running()) {
+        sleep(100_ms);
+    }
 }
 
 webserver::response_t WebControlResource::do_process_request(const webserver::request_t& request)
 {
-	log[log::info] << "Responding";
-//	auto prefix = request.url.path.substr(0, path_.size());
-//	if (prefix != path_) throw std::runtime_error("Wrong path");
-//	if ()
-	for (const auto&x : request.url.params) {
-//		log[log::info] << "Param: " << x.first << " -> " << x.second;
-		const auto& cmd = x.first;
-		const auto& val = x.second;
-		if (!val.empty()) {
-			log[log::info] << "Sending command " << cmd << " = " << val;
-			auto event = event::BasicEventParser::parse_expr(log,val,std::map<std::string,event::pBasicEvent>{});
-			if (!event) {
-				event = std::make_shared<event::EventString>(val);
-			}
-			emit_event(cmd, event);
-		} else {
-			log[log::info] << "Sending command " << cmd;
-			emit_event(cmd);
-		}
-	}
-	throw redirect_to(redirect_path_);
+    log[log::info] << "Responding";
+    for (const auto& x : request.url.params) {
+        const auto& cmd = x.first;
+        const auto& val = x.second;
+        if (!val.empty()) {
+            log[log::info] << "Sending command " << cmd << " = " << val;
+            auto event = event::BasicEventParser::parse_expr(log, val, std::map<std::string, event::pBasicEvent>{});
+            if (!event) {
+                event = std::make_shared<event::EventString>(val);
+            }
+            emit_event(cmd, event);
+        } else {
+            log[log::info] << "Sending command " << cmd;
+            emit_event(cmd);
+        }
+    }
+    throw redirect_to(redirect_path_);
 }
 bool WebControlResource::set_param(const core::Parameter& param)
 {
-	if (param.get_name() == "server_name") {
-		server_name_ = param.get<std::string>();
-	} else if (param.get_name() == "path") {
-		path_ = param.get<std::string>();
-	} else if (param.get_name() == "redirect") {
-		redirect_path_ = param.get<std::string>();
-	} else return core::IOThread::set_param(param);
-	return true;
+    if (assign_parameters(param)      //
+        (server_name_, "server_name") //
+        (path_, "path")               //
+        (redirect_path_, "redirect"))
+        return true;
+    return core::IOThread::set_param(param);
 }
 
 } /* namespace web_static */
--- src/modules/webserver/WebControlResource.h
+++ src/modules/webserver/WebControlResource.h
@@ -14,26 +14,22 @@
 namespace yuri {
 namespace webserver {
 
-class WebControlResource: public core::IOThread, public WebResource, public event::BasicEventProducer
-{
+class WebControlResource : public core::IOThread, public WebResource, public event::BasicEventProducer {
 public:
-	IOTHREAD_GENERATOR_DECLARATION
-	static core::Parameters configure();
-	WebControlResource(const log::Log &log_, core::pwThreadBase parent, const core::Parameters &parameters);
-	virtual ~WebControlResource() noexcept;
+    IOTHREAD_GENERATOR_DECLARATION
+    static core::Parameters configure();
+    WebControlResource(const log::Log& log_, core::pwThreadBase parent, const core::Parameters& parameters);
+    virtual ~WebControlResource() noexcept;
+
 private:
-
-	virtual void run() override;
-	virtual bool set_param(const core::Parameter& param) override;
-	virtual webserver::response_t do_process_request(const webserver::request_t& request) override;
-	std::string server_name_;
-	std::string path_;
-	std::string redirect_path_;
+    virtual void run() override;
+    virtual bool set_param(const core::Parameter& param) override;
+    virtual webserver::response_t do_process_request(const webserver::request_t& request) override;
+    std::string server_name_;
+    std::string path_;
+    std::string redirect_path_;
 };
-
 }
 }
-
-
 
 #endif /* SRC_MODULES_WEBSERVER_WEBCONTROLRESOURCE_H_ */
