6d93dfba0a39d806ca8279de46d10f03d21bc4f9
Zdenek Travnicek <v154c1@gmail.com>
2017-03-07 01:00:39 +0000

    Added option to change pipe types in yuri_simple

--- src/apps/simple/SimpleBuilder.cpp
+++ src/apps/simple/SimpleBuilder.cpp
@@ -21,20 +21,25 @@ std::string gen_name(const std::string& type, const std::string& cls, int i)
 	return type+"_"+cls+"_"+std::to_string(i);
 }
 
+struct parsed_parameter_t {
+	std::string class_name;
+	std::string name;
+	core::Parameters params;
+};
 /*!
- * Parses argument
+ * Parses an argument
  *
  * argument should be either just node name, or in the form:
- * name(param=value,param=value)
+ * name[param=value,param=value]
  *
  * @param log
  * @param arg
  * @param i
  * @return
  */
-core::node_record_t parse_argument(log::Log& log, const std::string& arg, int i)
+parsed_parameter_t parse_argument(log::Log& log, const std::string& arg, int i)
 {
-	core::node_record_t node;
+	parsed_parameter_t node;
 	boost::regex node_expr ("([a-zA-Z0-9_-]+)(\\[[^]]+\\])?");
 	boost::smatch what;
 	auto start = arg.cbegin();
@@ -46,7 +51,6 @@ core::node_record_t parse_argument(log::Log& log, const std::string& arg, int i)
 	node.class_name = std::string(what[1].first, what[1].second);
 	if (std::distance(what[2].first, what[2].second) > 2) {
 		std::map<std::string,event::pBasicEvent> var_cache;
-//		boost::regex param_line("([^=]+)=([^,]+)(,)?");
 
 		// supported param values:
 		//				"abcahdkashdlka ,)(0 "
@@ -65,11 +69,11 @@ core::node_record_t parse_argument(log::Log& log, const std::string& arg, int i)
 			const auto param_value = std::string(std::string(res[2].first,res[2].second));
 			++it;
 			if (auto parsed_event = event::BasicEventParser::parse_expr(log, param_value, var_cache)) {
-				node.parameters[param_name]= parsed_event;
+				node.params[param_name]= parsed_event;
 			} else {
-				node.parameters[param_name] = param_value;
+				node.params[param_name] = param_value;
 			}
-			var_cache[param_name]=node.parameters[param_name].get_value();
+			var_cache[param_name]=node.params[param_name].get_value();
 		}
 	}
 	node.name = gen_name("node", node.class_name, i);
@@ -90,13 +94,19 @@ SimpleBuilder::SimpleBuilder(const log::Log& log_, core::pwThreadBase parent, co
 
 	std::string last;
 	int i = 0;
+	parsed_parameter_t link_info{"single", {}, {}};
 	for (const auto& s: argv) {
-		auto node = parse_argument(log, s,++i);
+		if (s.substr(0,2) == "-p") {
+			link_info = parse_argument(log, s.substr(2), ++i);
+			continue;
+		}
+		auto param = parse_argument(log, s, ++i);
+		core::node_record_t node = {std::move(param.name), std::move(param.class_name), std::move(param.params), {}};
 		nodes[node.name]=node;//{name, node_cls, {}, {}};
 		if (!last.empty()) {
 			auto link_name = gen_name("link","single",i);
 			log[log::info] << "link " << link_name << " from " << last << " to " << node.name;
-			links[link_name]={link_name, "single", {}, last, node.name, 0, 0, {}};
+			links[link_name]={link_name, link_info.class_name, link_info.params, last, node.name, 0, 0, {}};
 		}
 
 		last = node.name;
