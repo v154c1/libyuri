95d24ba9408a646aea5d4550728bb748595f94a4
Zdenek Travnicek <v154c1@gmail.com>
2019-11-04 21:21:52 +0100

    Added RawAVFilePlaylist

--- src/modules/rawavfile/register.cpp
+++ src/modules/rawavfile/register.cpp
@@ -10,10 +10,13 @@
 #include "AVDecoder.h"
 #include "RawAVFile.h"
 #include "yuri/core/Module.h"
+#include "RawAVFilePlaylist.h"
+
 namespace yuri {
 
 MODULE_REGISTRATION_BEGIN("avmodules")
     REGISTER_IOTHREAD("rawavsource", rawavfile::RawAVFile)
     REGISTER_IOTHREAD("avdecoder", avdecoder::AVDecoder)
+    REGISTER_IOTHREAD("rawavplaylist", rawavfile::RawAVFilePlaylist)
 MODULE_REGISTRATION_END()
 }
